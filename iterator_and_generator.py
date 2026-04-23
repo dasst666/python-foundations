b5c38e11307ce34cda4859f1d70a1a03ddba0d45
Dastan <ddbolatkhan@gmail.com>
2026-04-23 17:35:23 +0500

    write another generators

--- iterator_and_generator.py
+++ iterator_and_generator.py
@@ -28,7 +28,7 @@ def my_generator(start, end):
 numbers = my_generator(0, 5)
 for number in numbers:
     print(number)
-
+# #################################################################
 class FibIterator:
     def __init__(self, num):
         self.num = num
@@ -57,3 +57,44 @@ def fib_generator(num):
 
 for i in fib_generator(5):
     print(i)
+# #################################################################
+from itertools import islice
+
+def chunked(iterable, size):
+    it = iter(iterable)
+    while True:
+       chunk = list(islice(it, size))
+       if not chunk:
+           break
+       yield chunk
+        
+
+for i in chunked([1, 2, 3, 4, 5, 6, 7], 3):
+    print(i)
+
+# #################################################################
+
+def take(iterable, n):
+    it = iter(iterable)
+    for _ in range(n):
+        try:
+            yield next(it)
+        except StopIteration:
+            return
+
+def take(iterable, n):
+    for _, value in zip(range(n), iterable):
+        yield value
+
+result = list(take([1, 2], 5))
+print(result)
+# #################################################################
+
+def drop(iterable, n):
+    it = iter(iterable)
+    yield from islice(it, n, None)
+        
+
+for i in drop([1, 2, 3, 4, 5, 6, 7], 3):
+    print(i)
+    
